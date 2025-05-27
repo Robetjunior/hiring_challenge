a395650aab924269f53f5c0a8d635c6c89ae2590
Robetjunior <juniorferreirarob@gmail.com>
2025-05-26 23:25:22 -0300

    feat(models): add bidirectional neighbors relation to Area entity
    - Introduced ManyToMany self-relation
    eighbors on Area model
    - Configured @JoinTable for rea_neighbors pivot table with reaId and
    eighborId
    - Allows representing mutual neighborhood between areas

--- backend/src/models/Area.ts
+++ backend/src/models/Area.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, ManyToMany, JoinTable } from "typeorm";
 import { Plant } from "./Plant";
 import { Equipment } from "./Equipment";
 
@@ -13,18 +13,26 @@ export class Area {
     @Column()
     locationDescription!: string;
 
-    @ManyToOne(() => Plant, plant => plant.areas)
+    @ManyToOne(() => Plant, (plant: { areas: any; }) => plant.areas)
     plant?: Plant;
 
     @Column()
     plantId!: string;
 
-    @OneToMany(() => Equipment, equipment => equipment.area)
+    @OneToMany(() => Equipment, (equipment: { area: any; }) => equipment.area)
     equipment?: Equipment[];
+
+    @ManyToMany(() => Area, (area: { neighbors: any; }) => area.neighbors)
+    @JoinTable({
+      name: 'area_neighbors',
+      joinColumn: { name: 'areaId', referencedColumnName: 'id' },
+      inverseJoinColumn: { name: 'neighborId', referencedColumnName: 'id' }
+    })
+    neighbors?: Area[];
 
     @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
     createdAt!: Date;
 
     @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP" })
     updatedAt!: Date;
-} 
+}
