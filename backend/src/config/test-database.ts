5bf8b892ef06bc20af2397efd99d5768ef9fc8dc
Robetjunior <juniorferreirarob@gmail.com>
2025-05-28 19:55:06 -0300

    feat(test-datasource): include Maintenance entity in test configuration
    - import Maintenance model
    - add Maintenance to the entities array in TestDataSource

--- backend/src/config/test-database.ts
+++ backend/src/config/test-database.ts
@@ -3,13 +3,14 @@ import { Plant } from "../models/Plant";
 import { Area } from "../models/Area";
 import { Equipment } from "../models/Equipment";
 import { Part } from "../models/Part";
+import { Maintenance } from "../models/Maintenance";
 
 export const TestDataSource = new DataSource({
     type: "sqlite",
     database: ":memory:",
     synchronize: true,
     logging: false,
-    entities: [Plant, Area, Equipment, Part],
+    entities: [Plant, Area, Equipment, Part, Maintenance],
     migrations: [],
     subscribers: [],
 }); 
